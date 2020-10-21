ce4026ea620a0d6bbe5c29fdc781968c795057b0
Patricia <prmonday@gmail.com>
2020-10-21 22:21:52 +0100

    first test FizzBuzz

---
+++ spec/FizzBuzz_spec.rb
@@ -0,0 +1,7 @@
+require 'FizzBuzz'
+describe 'FizzBuzz' do
+  it "takes a multiple of 5 or 3 as an input and return Fizz, Buzz or FizzBuzz" do
+    multiples = FizzBuzz.new
+    expect(FizzBuzz).to  eq(FizzBuzz)
+  end
+end
