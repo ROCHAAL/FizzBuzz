b7806d6a29c37c327997bc14e36798373477014c
Patricia <prmonday@gmail.com>
2020-10-23 11:32:41 +0100

    1st Refactoring Fizzbuzz

--- spec/FizzBuzz_spec.rb
+++ spec/FizzBuzz_spec.rb
@@ -1,12 +1,7 @@
   require 'FizzBuzz'
 
   describe 'FizzBuzz' do
-      it 'takes a multiple of 5 or 3 as an input and return Fizz, Buzz or FizzBuzz' do
-            fizz_buzz = FizzBuzz.new
-            expect(FizzBuzz).to  eq(FizzBuzz)
-          end
-
-            it  'takes a multiple of 3 as an input and return the string Fizz' do
+          it  'takes a multiple of 3 as an input and return the string Fizz' do
               fizz_buzz = FizzBuzz.new
               result = fizz_buzz.mutiple_of_three(3)
               expect(result).to eq('Fizz')
